9c9b6ad6d51d3a72f723c0941d0ddeb6f4a459da
MSNimo <mikebenim@gmail.com>
2017-11-01 18:44:40 -0500

    Initial Push Grid developed sort LMAO

---
+++ TDGame/Assets/Code/Cell.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cell : MonoBehaviour {
+
+    private Vector3 position;
+    private int row;
+    private int column;
+    private bool built = false;
+
+	// Use this for initialization
+	void Start () {
+        position = transform.position;
+        row = (int) position[2] / 8 + 2;
+        column = (int) position[0] / 8 + 2;
+        Debug.Log(row + " "+ column);
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
